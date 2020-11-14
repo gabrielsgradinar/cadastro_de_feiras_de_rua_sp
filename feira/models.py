56a4bb688b483407f6443172fd57eca6adedc021
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2020-11-14 08:41:04 -0300

    Criaçao do modelo Feira do banco de dados

--- feira/models.py
+++ feira/models.py
@@ -3,7 +3,15 @@ from db import db
 
 class Feira(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    registro = db.Column(db.String(10), unique=True, nullable=False)
+    nome = db.Column(db.String(80), unique=True, nullable=False)   
+    distrito = db.Column(db.String(80), nullable=False)
+    regiao = db.Column(db.String(80), nullable=False)    
+    logradouro = db.Column(db.String(10), nullable=False)
+    numero = db.Column(db.String(10), nullable=True)
+    bairro = db.Column(db.String(10), nullable=False)
+    referencia = db.Column(db.String(10), nullable=True)
+    
 
     def __repr__(self):
         return "<Feira %r>" % self.name
