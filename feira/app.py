30449d42f0c9c069809d18a53963d91b5a0b9ef5
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2020-11-14 20:10:37 -0300

    Mudança dos nomes de alguns arquivos e criaçao de todas a funçoes dos CRUD

--- feira/app.py
+++ feira/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import rotas
+import view
 import db
 import linha_de_comando
 
 
 def create_app():
     app = Flask(__name__)
-    rotas.init_app(app)
+    view.init_app(app)
     db.init_app(app)
     linha_de_comando.init_app(app)
 
--- feira/linha_de_comando.py
+++ feira/linha_de_comando.py
@@ -1,10 +1,10 @@
 import click
 from db import db
-import models
+import model
 
 
 def init_app(app):
     @app.cli.command()
     def create_db():
-        """Este comando inicializa o db"""
+        """Este comando inicializa o banco de dados"""
         db.create_all()
